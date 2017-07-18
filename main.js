16ac98fb64ac29cceca124dbc9971b2f03fe022c
Austin Cory Bart <acbart@vt.edu>
2017-07-18 12:13:22 -0400

    Bootstrap, jquery, knockoutjs working

--- main.js
+++ main.js
@@ -1,7 +1,27 @@
+// Mock DOM
 const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
-const { window } = new JSDOM(`<!DOCTYPE html>`);
-const $ = require('jquery')(window);
+const { window } = new JSDOM(`...`);
+global.window = window;
+const { document } = window.window;
+global.document = document;
+window.console = global.console;
+
+// Load JQuery
+$ = require('jquery')(window);
+global.jQuery = require('jquery');
+$ = jQuery;
+//global.jQuery.fn = {};
+
+var clonedInstanceMethods = {};
+for (var instancemethod in jQuery.prototype){
+    Object.assign(clonedInstanceMethods,jQuery.prototype[instancemethod])
+}
+
+// Knockout
+const ko = require('knockout');
+// Bootstrap
+const bootstrap = require('bootstrap');
 
 $('<h1>Hello</h1>').appendTo('body');
 console.log($('h1').text());
